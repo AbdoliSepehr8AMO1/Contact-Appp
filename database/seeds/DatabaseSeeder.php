416be72a0fbea33b7672c4333299158295ffc923
sepehr abdoli <sepehrskater@yahoo.com>
2020-08-23 17:59:25 +0200

    resoruce code toegevoegd

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call([
+            RoleTableSeeder::class,
+            UserTableSeeder::class,
+            PostTableSeeder::class,
+        ]);
+    }
+
+
+}
